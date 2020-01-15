f8959b99ce3352f9623c961446dc4ab5a52413b5
Phenecy <phenecy.dev@gmail.com>
2020-01-15 09:07:39 +0300

    fix(api): fix naming

--- app/src/main/java/dev/phenecy/poketop/api/RetrofitService.kt
+++ app/src/main/java/dev/phenecy/poketop/api/RetrofitService.kt
@@ -6,7 +6,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-public interface RetrofitService {
+interface RetrofitService {
 
     @GET("pokemon/{id}")
     fun getPokemon(@Path("id") id: Int): Call<Pokemon>
